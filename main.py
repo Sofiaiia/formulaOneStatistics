2db8ef80857e16c9ae2c9320a68be0820dfdfee9
sofia södergårdh <sofia.s@aland.net>
2022-04-19 14:43:18 +0300

    ändrat lite funktionerna

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def circuitsMap():
         folium.Marker(
             location=[row['lat'], row['lng']],
             tooltip=row['name'],
-            icon=folium.Icon(color='#FF1801', prefix='fa', icon='flag-checkered',icon_color='white')
+            icon=folium.Icon(color='red', prefix='fa', icon='flag-checkered')
         ).add_to(map_cluster)
 
     circuits_map.save('mymap.html')
@@ -102,11 +102,13 @@ def racesPerSeason():
     plt.show()
 
 def speedVisualization():
-
-    year = 2021
-    wknd = 14
+    year = input("Enter the year: ")
+    year = int(year)
+    wknd = input("Weekend number: ")
+    wknd = int(wknd)
+    driver = input("Which driver, use shortname(ex: VER): ")
     ses = 'R'
-    driver = 'VER'
+
     colormap = mpl.cm.plasma
 
     session = ff1.get_session(year, wknd, ses)
@@ -114,43 +116,30 @@ def speedVisualization():
     session.load()
     lap = session.laps.pick_driver(driver).pick_fastest()
 
-    # Get telemetry data
-    x = lap.telemetry['X']              # values for x-axis
-    y = lap.telemetry['Y']              # values for y-axis
-    color = lap.telemetry['Speed']      # value to base color gradient on
-
+    x = lap.telemetry['X']
+    y = lap.telemetry['Y']
+    color = lap.telemetry['Speed']
     points = np.array([x, y]).T.reshape(-1, 1, 2)
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-    # We create a plot with title and adjust some setting to make it look good.
     fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
     fig.suptitle(f'{weekend.name} {year}', size=24, y=0.97)
 
-    # Adjust margins and turn of axis
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
     ax.axis('off')
 
-    # Create background track line
     ax.plot(lap.telemetry['X'], lap.telemetry['Y'], color='black', linestyle='-', linewidth=16, zorder=0)
 
-    # Create a continuous norm to map from data points to colors
     norm = plt.Normalize(color.min(), color.max())
     lc = LineCollection(segments, cmap=colormap, norm=norm, linestyle='-', linewidth=5)
 
-    # Set the values used for colormapping
     lc.set_array(color)
-
-    # Merge all line segments together
     line = ax.add_collection(lc)
 
-
-    # Finally, we create a color bar as a legend.
     cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
     normlegend = mpl.colors.Normalize(vmin=color.min(), vmax=color.max())
     legend = mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=colormap, orientation="horizontal")
 
-
-    # Show the plot
     plt.show()
 
 def fastestEachSeason():
